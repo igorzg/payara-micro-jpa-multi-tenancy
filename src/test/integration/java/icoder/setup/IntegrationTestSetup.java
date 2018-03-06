f068154b6103dfb4a138b0b1fc16e8b0006c6ce5
igorzg <igor.zg1987@gmail.com>
2018-03-06 23:00:43 +0100

    Update of ejb jar

--- src/test/integration/java/icoder/setup/IntegrationTestSetup.java
+++ src/test/integration/java/icoder/setup/IntegrationTestSetup.java
@@ -4,16 +4,22 @@ import icoder.RestApplication;
 import org.eu.ingwar.tools.arquillian.extension.suite.annotations.ArquillianSuiteDeployment;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.formatter.Formatters;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 
 @ArquillianSuiteDeployment
 public class IntegrationTestSetup {
-  @Deployment
-  public static WebArchive createDeployment() {
-    return ShrinkWrap.create(WebArchive.class)
-         .addPackages(true, RestApplication.class.getPackage())
-        .addAsWebInfResource("beans.xml")
-        .addAsResource("log4j2.xml")
-        .addAsResource("config.properties");
-  }
+    @Deployment
+    public static WebArchive createDeployment() {
+        WebArchive webArchive = ShrinkWrap.create(WebArchive.class)
+                .addPackages(true, RestApplication.class.getPackage())
+                .addAsResource("persistence.xml","META-INF/persistence.xml")
+                .addAsWebInfResource("beans.xml")
+                .addAsWebInfResource("ejb-jar.xml")
+                .addAsWebInfResource("glassfish-resources.xml")
+                .addAsResource("log4j2.xml")
+                .addAsResource("config.properties");
+        System.out.println(webArchive.toString(Formatters.VERBOSE));
+        return webArchive;
+    }
 }
